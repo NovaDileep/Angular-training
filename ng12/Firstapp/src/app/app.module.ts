806756613d8c30cdcd130f1add20897aec614c13
Nova Dileep <novadileep123@gmail.com>
2021-10-22 17:03:12 +0530

    changed home name to dashboard

--- ng12/Firstapp/src/app/app.module.ts
+++ ng12/Firstapp/src/app/app.module.ts
@@ -6,7 +6,6 @@ import { FormsModule } from '@angular/forms';
 import { NgChartsModule } from 'ng2-charts';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { NavComponent } from './nav/nav.component';
@@ -18,7 +17,6 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
     LoginComponent,
     RegisterComponent,
     NavComponent,
